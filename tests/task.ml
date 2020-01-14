59d36f0902d1d78cb865647a87eeb6db02816921
Darren Ldl <darrenldldev@gmail.com>
2020-01-14 16:21:13 +1100

    Code refactoring

--- tests/task.ml
+++ tests/task.ml
@@ -1,9 +1,18 @@
 open Test_utils
 
+(*$ #use "tests/unpack_pack.cinaps";;
+
+  print_unpack_is_inverse_of_pack_test ~typ:"arith_seq"
+    ~f_pack_name:"Daypack_lib.Task.Serialize.pack_arith_seq"
+    ~f_unpack_name:"Daypack_lib.Task.Deserialize.unpack_arith_seq";
+*)
+
 let qc_unpack_is_inverse_of_pack_arith_seq =
   QCheck.Test.make ~count:10_000 ~name:"qc_unpack_is_inverse_of_pack_arith_seq"
-    arith_seq (fun arith_seq ->
-        arith_seq |> Daypack_lib.Task.Serialize.pack_arith_seq
-        |> Daypack_lib.Task.Deserialize.unpack_arith_seq = arith_seq)
+    arith_seq (fun x ->
+        x |> Daypack_lib.Task.Serialize.pack_arith_seq
+        |> Daypack_lib.Task.Deserialize.unpack_arith_seq = x)
+
+(*$*)
 
 let suite = [ qc_unpack_is_inverse_of_pack_arith_seq ]
