aebdde001cce4e89abfb728632fcb567e7513701
Darren Ldl <darrenldldev@gmail.com>
2020-01-15 16:26:04 +1100

    Adjusting test round

--- tests/sched.ml
+++ tests/sched.ml
@@ -135,7 +135,7 @@ open Test_utils
 *)
 
 let qc_unpack_is_inverse_of_pack_task_store =
-  QCheck.Test.make ~count:1000 ~name:"qc_unpack_is_inverse_of_pack_task_store"
+  QCheck.Test.make ~count:5000 ~name:"qc_unpack_is_inverse_of_pack_task_store"
     QCheck.(list_of_size Gen.(int_bound 100) task)
     (fun l ->
        let x = l |> List.to_seq |> Daypack_lib.Task_id_map.of_seq in
@@ -146,7 +146,7 @@ let qc_unpack_is_inverse_of_pack_task_store =
        Daypack_lib.Task_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_task_inst_store =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_task_inst_store"
     QCheck.(list_of_size Gen.(int_bound 100) task_inst)
     (fun l ->
@@ -158,7 +158,7 @@ let qc_unpack_is_inverse_of_pack_task_inst_store =
        Daypack_lib.Task_inst_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_task_seg_store =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_task_seg_store"
     QCheck.(list_of_size Gen.(int_bound 100) task_seg)
     (fun l ->
@@ -170,7 +170,7 @@ let qc_unpack_is_inverse_of_pack_task_seg_store =
        Daypack_lib.Task_seg_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_sched_req_pending_store =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_sched_req_pending_store"
     QCheck.(list_of_size Gen.(int_bound 100) sched_req)
     (fun l ->
@@ -182,7 +182,7 @@ let qc_unpack_is_inverse_of_pack_sched_req_pending_store =
        Daypack_lib.Sched_req_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_sched_req_record_store =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_sched_req_record_store"
     QCheck.(list_of_size Gen.(int_bound 100) sched_req_record)
     (fun l ->
@@ -194,7 +194,7 @@ let qc_unpack_is_inverse_of_pack_sched_req_record_store =
        Daypack_lib.Sched_req_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_quota =
-  QCheck.Test.make ~count:1000 ~name:"qc_unpack_is_inverse_of_pack_quota"
+  QCheck.Test.make ~count:5000 ~name:"qc_unpack_is_inverse_of_pack_quota"
     QCheck.(list_of_size Gen.(int_bound 100) (pair task_inst_id pos_int64))
     (fun l ->
        let x = l |> List.to_seq |> Daypack_lib.Task_inst_id_map.of_seq in
@@ -205,7 +205,7 @@ let qc_unpack_is_inverse_of_pack_quota =
        Daypack_lib.Task_inst_id_map.equal (fun x y -> compare x y = 0) x y)
 
 let qc_unpack_is_inverse_of_pack_user_id_to_task_ids =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_user_id_to_task_ids"
     QCheck.(list_of_size Gen.(int_bound 10) (pair pos_int64 pos_int64_set))
     (fun l ->
@@ -217,7 +217,7 @@ let qc_unpack_is_inverse_of_pack_user_id_to_task_ids =
        Daypack_lib.User_id_map.equal Daypack_lib.Int64_set.equal x y)
 
 let qc_unpack_is_inverse_of_pack_task_id_to_task_inst_ids =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_task_id_to_task_inst_ids"
     QCheck.(list_of_size Gen.(int_bound 10) (pair task_id pos_int64_set))
     (fun l ->
@@ -229,7 +229,7 @@ let qc_unpack_is_inverse_of_pack_task_id_to_task_inst_ids =
        Daypack_lib.Task_id_map.equal Daypack_lib.Int64_set.equal x y)
 
 let qc_unpack_is_inverse_of_pack_task_inst_id_to_task_seg_ids =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_task_inst_id_to_task_seg_ids"
     QCheck.(list_of_size Gen.(int_bound 10) (pair task_inst_id pos_int64_set))
     (fun l ->
@@ -241,7 +241,7 @@ let qc_unpack_is_inverse_of_pack_task_inst_id_to_task_seg_ids =
        Daypack_lib.Task_inst_id_map.equal Daypack_lib.Int64_set.equal x y)
 
 let qc_unpack_is_inverse_of_pack_sched_req_ids =
-  QCheck.Test.make ~count:1000
+  QCheck.Test.make ~count:5000
     ~name:"qc_unpack_is_inverse_of_pack_sched_req_ids" pos_int64_set (fun x ->
         let y =
           x |> Daypack_lib.Sched.Serialize.pack_sched_req_ids
