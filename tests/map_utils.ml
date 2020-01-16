35171eb18b4e4ef196d3d96ae19002233a36a581
Darren Ldl <darrenldldev@gmail.com>
2020-01-16 22:06:17 +1100

    Adding diff tests

--- tests/map_utils.ml
+++ tests/map_utils.ml
@@ -7,23 +7,66 @@ open Test_utils
      "task_store",
      "Daypack_lib.Task_id_map_utils.diff",
      "Daypack_lib.Task_id_map_utils.add_diff",
+     "Daypack_lib.Task_id_map_utils.sub_diff",
      "Daypack_lib.Task_id_map.equal"
     )
   ]
   in
 
-  List.iter (fun (name, store_gen, f_diff, f_add_diff, f_equal) ->
+  List.iter (fun (name, store_gen, f_diff, f_add_diff, _f_sub_diff, f_equal) ->
       print_add_diff_test
         ~name
         ~store_gen
         ~f_diff
         ~f_add_diff
-        ~f_equal)
+        ~f_equal;
+    )
+    store_list;
+
+  List.iter (fun (name, store_gen, f_diff, _f_add_diff, f_sub_diff, f_equal) ->
+      print_sub_diff_test
+        ~name
+        ~store_gen
+        ~f_diff
+        ~f_sub_diff
+        ~f_equal;
+    )
+    store_list;
+
+  List.iter (fun (name, store_gen, f_diff, f_add_diff, f_sub_diff, f_equal) ->
+      print_sub_diff_is_inverse_of_add_diff_test
+        ~name
+        ~store_gen
+        ~f_diff
+        ~f_add_diff
+        ~f_sub_diff
+        ~f_equal;
+    )
+    store_list;
+
+  List.iter (fun (name, store_gen, f_diff, f_add_diff, f_sub_diff, f_equal) ->
+      print_add_diff_is_inverse_of_sub_diff_test
+        ~name
+        ~store_gen
+        ~f_diff
+        ~f_add_diff
+        ~f_sub_diff
+        ~f_equal;
+    )
     store_list;
 
   print_endline "let suite = [";
-  List.iter (fun (name, _, _, _, _) ->
+  List.iter (fun (name, _, _, _, _, _) ->
       Printf.printf "%s;\n" (get_add_diff_test_name name);
+    ) store_list;
+  List.iter (fun (name, _, _, _, _, _) ->
+      Printf.printf "%s;\n" (get_sub_diff_test_name name);
+    ) store_list;
+  List.iter (fun (name, _, _, _, _, _) ->
+      Printf.printf "%s;\n" (get_sub_diff_is_inverse_of_add_diff_test_name name);
+    ) store_list;
+  List.iter (fun (name, _, _, _, _, _) ->
+      Printf.printf "%s;\n" (get_add_diff_is_inverse_of_sub_diff_test_name name);
     ) store_list;
   print_endline "]"
 *)
@@ -38,6 +81,46 @@ let add_diff_test_task_store =
          (Daypack_lib.Task_id_map_utils.add_diff diff old)
          x)
 
-let suite = [ add_diff_test_task_store ]
+let sub_diff_test_task_store =
+  QCheck.Test.make ~count:5000 ~name:"sub_diff_test_task_store"
+    QCheck.(pair task_store task_store)
+    (fun (old, x) ->
+       let diff = Daypack_lib.Task_id_map_utils.diff ~old x in
+       Daypack_lib.Task_id_map.equal
+         (fun x y -> compare x y = 0)
+         (Daypack_lib.Task_id_map_utils.sub_diff diff x)
+         old)
+
+let sub_diff_is_inverse_of_add_diff_test_task_store =
+  QCheck.Test.make ~count:5000
+    ~name:"sub_diff_is_inverse_of_add_diff_test_task_store"
+    QCheck.(pair task_store task_store)
+    (fun (old, x) ->
+       let diff = Daypack_lib.Task_id_map_utils.diff ~old x in
+       Daypack_lib.Task_id_map.equal
+         (fun x y -> compare x y = 0)
+         (Daypack_lib.Task_id_map_utils.sub_diff diff
+            (Daypack_lib.Task_id_map_utils.add_diff diff old))
+         old)
+
+let add_diff_is_inverse_of_sub_diff_test_task_store =
+  QCheck.Test.make ~count:5000
+    ~name:"add_diff_is_inverse_of_sub_diff_test_task_store"
+    QCheck.(pair task_store task_store)
+    (fun (old, x) ->
+       let diff = Daypack_lib.Task_id_map_utils.diff ~old x in
+       Daypack_lib.Task_id_map.equal
+         (fun x y -> compare x y = 0)
+         (Daypack_lib.Task_id_map_utils.sub_diff diff
+            (Daypack_lib.Task_id_map_utils.add_diff diff old))
+         old)
+
+let suite =
+  [
+    add_diff_test_task_store;
+    sub_diff_test_task_store;
+    sub_diff_is_inverse_of_add_diff_test_task_store;
+    add_diff_is_inverse_of_sub_diff_test_task_store;
+  ]
 
 (*$*)
