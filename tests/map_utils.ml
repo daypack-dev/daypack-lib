bfe7d9767a40799d517d1203ac39e139391d20cd
Darren Ldl <darrenldldev@gmail.com>
2020-01-17 02:42:35 +1100

    Progress on bucket diff tests

--- tests/map_utils.ml
+++ tests/map_utils.ml
@@ -83,19 +83,44 @@ open Test_utils
     )
     store_list;
 
+  let bucket_store_list = [
+    ("user_id_to_task_ids",
+     "user_id_to_task_ids",
+     "Daypack_lib.User_id_map_utils.Int64_bucketed.diff_bucketed",
+     "Daypack_lib.User_id_map_utils.Int64_bucketed.add_diff_bucketed",
+     "Daypack_lib.User_id_map_utils.Int64_bucketed.sub_diff_bucketed",
+     "Daypack_lib.User_id_map.equal",
+     "Daypack_lib.Int64_set.equal"
+    );
+  ] in
+
+  List.iter (fun (name, store_gen, f_diff, f_add_diff, _f_sub_diff, f_equal, f_bucket_equal) ->
+      Diff_bucketed.print_add_diff_bucketed_test
+        ~name
+        ~store_gen
+        ~f_diff
+        ~f_add_diff
+        ~f_equal
+        ~f_bucket_equal;
+    )
+    bucket_store_list;
+
   print_endline "let suite = [";
-  List.iter (fun (name, _, _, _, _, _) ->
-      Printf.printf "%s;\n" (Diff.get_add_diff_test_name name);
-    ) store_list;
-  List.iter (fun (name, _, _, _, _, _) ->
-      Printf.printf "%s;\n" (Diff.get_sub_diff_test_name name);
-    ) store_list;
-  List.iter (fun (name, _, _, _, _, _) ->
-      Printf.printf "%s;\n" (Diff.get_sub_diff_is_inverse_of_add_diff_test_name name);
-    ) store_list;
-  List.iter (fun (name, _, _, _, _, _) ->
-      Printf.printf "%s;\n" (Diff.get_add_diff_is_inverse_of_sub_diff_test_name name);
-    ) store_list;
+  (* List.iter (fun (name, _, _, _, _, _) ->
+   *     Printf.printf "%s;\n" (Diff.get_add_diff_test_name name);
+   *   ) store_list;
+   * List.iter (fun (name, _, _, _, _, _) ->
+   *     Printf.printf "%s;\n" (Diff.get_sub_diff_test_name name);
+   *   ) store_list;
+   * List.iter (fun (name, _, _, _, _, _) ->
+   *     Printf.printf "%s;\n" (Diff.get_sub_diff_is_inverse_of_add_diff_test_name name);
+   *   ) store_list;
+   * List.iter (fun (name, _, _, _, _, _) ->
+   *     Printf.printf "%s;\n" (Diff.get_add_diff_is_inverse_of_sub_diff_test_name name);
+   *   ) store_list; *)
+  List.iter (fun (name, _, _, _, _, _, _) ->
+      Printf.printf "%s;\n" (Diff_bucketed.get_add_diff_bucketed_test_name name);
+    ) bucket_store_list;
   print_endline "]"
 *)
 
@@ -319,28 +344,19 @@ let add_diff_is_inverse_of_sub_diff_test_sched_req_record_store =
             (Daypack_lib.Sched_req_id_map_utils.add_diff diff old))
          old)
 
-let suite =
-  [
-    add_diff_test_task_store;
-    add_diff_test_task_inst_store;
-    add_diff_test_task_seg_store;
-    add_diff_test_sched_req_pending_store;
-    add_diff_test_sched_req_record_store;
-    sub_diff_test_task_store;
-    sub_diff_test_task_inst_store;
-    sub_diff_test_task_seg_store;
-    sub_diff_test_sched_req_pending_store;
-    sub_diff_test_sched_req_record_store;
-    sub_diff_is_inverse_of_add_diff_test_task_store;
-    sub_diff_is_inverse_of_add_diff_test_task_inst_store;
-    sub_diff_is_inverse_of_add_diff_test_task_seg_store;
-    sub_diff_is_inverse_of_add_diff_test_sched_req_pending_store;
-    sub_diff_is_inverse_of_add_diff_test_sched_req_record_store;
-    add_diff_is_inverse_of_sub_diff_test_task_store;
-    add_diff_is_inverse_of_sub_diff_test_task_inst_store;
-    add_diff_is_inverse_of_sub_diff_test_task_seg_store;
-    add_diff_is_inverse_of_sub_diff_test_sched_req_pending_store;
-    add_diff_is_inverse_of_sub_diff_test_sched_req_record_store;
-  ]
+let add_diff_bucketed_test_user_id_to_task_ids =
+  QCheck.Test.make ~count:5000
+    ~name:"add_diff_bucketed_test_user_id_to_task_ids"
+    QCheck.(pair user_id_to_task_ids user_id_to_task_ids)
+    (fun (old, x) ->
+       let diff =
+         Daypack_lib.User_id_map_utils.Int64_bucketed.diff_bucketed ~old x
+       in
+       Daypack_lib.User_id_map.equal Daypack_lib.Int64_set.equal
+         (Daypack_lib.User_id_map_utils.Int64_bucketed.add_diff_bucketed diff
+            old)
+         x)
+
+let suite = [ add_diff_bucketed_test_user_id_to_task_ids ]
 
 (*$*)
