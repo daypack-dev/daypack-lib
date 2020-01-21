df21007f4e1158aa9a72d09fa8b6493ecf11da4f
Darren Ldl <darrenldldev@gmail.com>
2020-01-22 00:50:07 +1100

    Commented out time pattern code

--- lib/time_pattern.ml
+++ lib/time_pattern.ml
@@ -19,22 +19,22 @@ let tm_to_time (tm : Unix.tm) : int64 =
   let time, _ = Unix.mktime tm in
   (time |> Int64.of_float) /^ 60L
 
-let jump_to_next_match (t : t) (time : int64) : bool =
-  let tm = time_to_tm time in
-  match t.min with
-  | Some x ->
-    
-  | None 
-
-let time_patches_pattern (t : t) (time : int64) : bool =
-  let tm =   in
-
-let matching_time_slots  (t : t) (time_slots : Time_slot.t Seq.t) : Time_slot.t Seq.t =
-  (* assume 1 time unit in time slot = 1 minute *)
-  let rec aux t time_slots =
-    match time_slots () with
-    | Seq.Nil -> Seq.empty
-    | Seq.Cons ((start, end_exc), rest) ->
-
-  in
-  aux t time_slots
+(* let jump_to_next_match (t : t) (time : int64) : bool =
+ *   let tm = time_to_tm time in
+ *   match t.min with
+ *   | Some x ->
+ *     
+ *   | None 
+ * 
+ * let time_patches_pattern (t : t) (time : int64) : bool =
+ *   let tm =   in
+ * 
+ * let matching_time_slots  (t : t) (time_slots : Time_slot.t Seq.t) : Time_slot.t Seq.t =
+ *   (\* assume 1 time unit in time slot = 1 minute *\)
+ *   let rec aux t time_slots =
+ *     match time_slots () with
+ *     | Seq.Nil -> Seq.empty
+ *     | Seq.Cons ((start, end_exc), rest) ->
+ * 
+ *   in
+ *   aux t time_slots *)
