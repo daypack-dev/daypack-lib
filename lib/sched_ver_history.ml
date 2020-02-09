ec89995071fc7cfad7800f886fc3fd8e5c048069
Darren Ldl <darrenldldev@gmail.com>
2020-02-09 15:37:54 +1100

    Progress on sched_ver_history

--- lib/sched_ver_history.ml
+++ lib/sched_ver_history.ml
@@ -1,5 +1,10 @@
 type t = { mutable history : Sched.sched list }
 
+type head_choice = [
+  | `In_place
+  | `New_head
+]
+
 (* let fold_head ~none:(f_none : unit -> Sched.sched)
  *     ~some:(f : Sched.sched -> Sched.sched) (t : t) : unit =
  *   match t.history with
@@ -8,12 +13,17 @@ type t = { mutable history : Sched.sched list }
  *     let hd = f hd in
  *     t.history <- hd :: tl *)
 
-let map_head (f : Sched.sched -> 'a * Sched.sched) (t : t) : 'a =
+let map_head (f : Sched.sched -> 'a * head_choice * Sched.sched) (t : t) : 'a =
   match t.history with
-  | [] -> let ret, sched = f Sched.empty in t.history <- [ sched ]; ret
+  | [] -> let ret, _, sched = f Sched.empty in t.history <- [ sched ]; ret
   | hd :: tl ->
-    let ret, hd = f hd in
-    t.history <- hd :: tl;
+    let ret, choice, x = f hd in
+    (match choice with
+     | `In_place ->
+       t.history <- x :: tl
+     | `New_head ->
+       t.history <- x :: hd :: tl
+    );
     ret
 
 module In_place_head = struct
@@ -22,24 +32,44 @@ module In_place_head = struct
     map_head
       (fun sched ->
           let task, _, sched =
-            sched |>
             Sched.Task_store.add_task ~parent_user_id data task_inst_data_list
+              sched
           in
-          task, sched
+          task, `In_place, sched
       )
       t
 
   let queue_sched_req (data : Sched_req.sched_req_data) (t : t) : Sched_req.sched_req =
     map_head
       (fun sched ->
-         sched |>
+         let sched_req, sched =
          Sched.Sched_req_store.queue_sched_req_data data
+           sched
+         in
+         sched_req, `In_place, sched
       )
       t
 end
 
 module Maybe_append_to_head = struct
-  (* let remove_task_seg_place_by_ *)
+  let remove_task (task_id : Task.task_id) (t : t) : unit =
+    match t.history with
+    | [] -> ()
+    | hd :: tl ->
+      let task_seg_place_seq =
+        Sched.Task_seg_place_map.find_task_seg_place_seq_by_task_id task_id hd
+      in
+      match task_seg_place_seq () with
+      | Seq.Nil ->
+        let hd = Sched.Task_store.remove_task task_id hd in
+        t.history <- hd :: tl
+      | _ ->
+        let hd =
+          hd
+          |> Sched.Task_store.remove_task task_id
+          |> Sched.Task_seg_place_map.remove_task_seg_place_seq task_seg_place_seq
+        in
+        t.history <- hd :: tl
 end
 
 module Append_to_head = struct
