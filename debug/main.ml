11ded898d2f25ed10898319282f1df40f2c030fa
Darren Ldl <darrenldldev@gmail.com>
2020-02-11 02:35:07 +1100

    Adjusting debug print

--- debug/main.ml
+++ debug/main.ml
@@ -472,6 +472,10 @@ let debug_sched_usage_simulation () =
                  } ));
       }
     [] sched_ver_history;
+  add_task ~parent_user_id:0L
+    Task.{ splittable = false; parallelizable = false; task_type = One_off }
+    Task.[ { task_inst_type = Reminder } ]
+    sched_ver_history;
   List.iter
     (fun sched_req_data ->
        Sched_ver_history.In_place_head.queue_sched_req sched_req_data
@@ -481,16 +485,16 @@ let debug_sched_usage_simulation () =
       [
         Split_even
           {
-            task_seg_related_data = ((0L, 0L, 0L), 20L);
+            task_seg_related_data = ((0L, 2L, 0L), 20L);
             time_slots = [ (0L, 50L) ];
             buckets = [ (0L, 10L); (10L, 20L) ];
             incre = 1L;
           };
         Split_even
           {
-            task_seg_related_data = ((0L, 0L, 0L), 20L);
+            task_seg_related_data = ((0L, 2L, 0L), 20L);
             time_slots = [ (50L, 100L) ];
-            buckets = [ (50L, 60L); (60L, 70L) ];
+            buckets = [ (50L, 60L); (60L, 70L); (90L, 100L) ];
             incre = 1L;
           };
       ];
@@ -510,44 +514,44 @@ let debug_sched_usage_simulation () =
             incre = 1L;
           };
       ];
-      [
-        Split_and_shift
-          {
-            task_seg_related_data = ((0L, 0L, 2L), 15L);
-            time_slots = [ (50L, 150L) ];
-            split_count = Max_split 5L;
-            incre = 1L;
-            min_seg_size = 2L;
-            max_seg_size = None;
-          };
-      ];
-      [
-        Time_share
-          {
-            task_seg_related_data_list =
-              [ ((0L, 0L, 2L), 30L); ((0L, 0L, 3L), 20L) ];
-            time_slots = [ (50L, 200L) ];
-            interval_size = 30L;
-          };
-      ];
-      [
-        Push_toward
-          {
-            task_seg_related_data = ((0L, 0L, 4L), 10L);
-            target = 100L;
-            time_slots = [ (0L, 200L) ];
-            incre = 1L;
-          };
-      ];
-      [
-        Push_toward
-          {
-            task_seg_related_data = ((0L, 0L, 5L), 10L);
-            target = 75L;
-            time_slots = [ (0L, 200L) ];
-            incre = 1L;
-          };
-      ];
+      (* [
+       *   Split_and_shift
+       *     {
+       *       task_seg_related_data = ((0L, 0L, 2L), 15L);
+       *       time_slots = [ (50L, 150L) ];
+       *       split_count = Max_split 5L;
+       *       incre = 1L;
+       *       min_seg_size = 2L;
+       *       max_seg_size = None;
+       *     };
+       * ]; *)
+      (* [
+       *   Time_share
+       *     {
+       *       task_seg_related_data_list =
+       *         [ ((0L, 0L, 2L), 30L); ((0L, 0L, 3L), 20L) ];
+       *       time_slots = [ (50L, 200L) ];
+       *       interval_size = 30L;
+       *     };
+       * ]; *)
+      (* [
+       *   Push_toward
+       *     {
+       *       task_seg_related_data = ((0L, 0L, 4L), 10L);
+       *       target = 100L;
+       *       time_slots = [ (0L, 200L) ];
+       *       incre = 1L;
+       *     };
+       * ];
+       * [
+       *   Push_toward
+       *     {
+       *       task_seg_related_data = ((0L, 0L, 5L), 10L);
+       *       target = 75L;
+       *       time_slots = [ (0L, 200L) ];
+       *       incre = 1L;
+       *     };
+       * ]; *)
     ];
   Sched_ver_history.Print.debug_print_sched_ver_history sched_ver_history;
   print_endline "=====";
