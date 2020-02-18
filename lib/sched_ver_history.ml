aa57b7d832affeaafbb6d371c82b7b5b5fb949e6
Darren Ldl <darrenldldev@gmail.com>
2020-02-19 02:16:25 +1100

    Added call to leftover handling function in Sched_ver_history

--- lib/sched_ver_history.ml
+++ lib/sched_ver_history.ml
@@ -186,6 +186,7 @@ module Maybe_append_to_head = struct
         let sched_req_records, hd' =
           hd
           |> Sched.Recur.instantiate ~start ~end_exc
+          |> Sched.Leftover.sched_for_leftover_task_segs ~start ~end_exc
           |> Sched.Sched_req.allocate_task_segs_for_pending_sched_reqs ~start
             ~end_exc ~include_sched_reqs_partially_within_time_period
             ~up_to_sched_req_id_inc
