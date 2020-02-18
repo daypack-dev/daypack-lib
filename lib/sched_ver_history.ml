3c21bce7d8621d4b6c7aebc0dba62d21acf5bc5e
Darren Ldl <darrenldldev@gmail.com>
2020-02-18 23:43:15 +1100

    Formatting

--- lib/sched_ver_history.ml
+++ lib/sched_ver_history.ml
@@ -161,7 +161,8 @@ module Maybe_append_to_head = struct
           |> Sched.Task_inst.remove_task_inst task_inst_id
           |> Sched.Sched_req.remove_pending_sched_req_by_task_inst_id
             task_inst_id
-          |> Sched.Sched_req.remove_sched_req_record_by_task_inst_id task_inst_id
+          |> Sched.Sched_req.remove_sched_req_record_by_task_inst_id
+            task_inst_id
         in
         let task_seg_place_seq =
           Sched.Agenda.find_task_seg_place_seq_by_task_inst_id task_inst_id hd
