caaa50812ed07b152d23e296c50a3a3ae3c8147b
Darren Ldl <darrenldldev@gmail.com>
2020-02-23 00:56:58 +1100

    Formatting

--- debug/main.ml
+++ debug/main.ml
@@ -581,8 +581,8 @@ let debug_sched_usage_simulation () =
   print_endline "=====";
   print_endline "Recording prgress";
   Sched_ver_history.Print.debug_print_sched_ver_history sched_ver_history;
-  Sched_ver_history.In_place_head.move_task_seg_to_completed (0L, 1L, 0L, 0L, None)
-    sched_ver_history;
+  Sched_ver_history.In_place_head.move_task_seg_to_completed
+    (0L, 1L, 0L, 0L, None) sched_ver_history;
   (* Sched_ver_history.In_place_head.move_task_inst_to_completed (0L, 1L, 0L)
    *   sched_ver_history; *)
   Sched_ver_history.In_place_head.add_task_seg_progress_chunk
