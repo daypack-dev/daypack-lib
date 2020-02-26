8112fe2417903584295b9755cb485fe3f7122302
Darren Ldl <darrenldldev@gmail.com>
2020-02-26 21:47:55 +1100

    Progress on time_profile

--- lib/time_profile.ml
+++ lib/time_profile.ml
@@ -2,13 +2,28 @@ type period = Time_pattern.t * Time_pattern.t
 
 type data = { periods : period list }
 
+(* let matching_tm_seq_of_periods ~search_years_ahead (start : Unix.tm) :
+ *   (Unix.tm Seq.t =
+ *   periods
+ *   |> List.to_seq
+ *   |> Seq.map (fun (start_pat, end_exc_pat) ->
+ *       let start_seq =
+ *         Time_pattern.matching_tm_seq ~search_years_ahead start_pat start
+ *       in
+ *       let end_exc_seq =
+ *         Time_pattern.matching_tm_seq ~search_years_ahead end_exc_pat start
+ *       in
+ *       OSeq.map2 (fun start end_exc -> (start, end_exc)) start_seq end_exc_seq
+ *     )
+ * |> OSeq.merge *)
+
 let matching_time_slots_of_periods ~start ~end_exc (periods : period list) :
   Time_slot_ds.t Seq.t =
   let time_slots = [ (start, end_exc) ] in
   periods
   |> List.to_seq
   |> Seq.map (fun (start_pat, end_exc_pat) ->
-      ( let start_seq =
+      let start_seq =
           Time_pattern.matching_time_slots start_pat time_slots
           |> Seq.map (fun (x, _) -> x)
         in
@@ -19,5 +34,5 @@ let matching_time_slots_of_periods ~start ~end_exc (periods : period list) :
         OSeq.map2
           (fun start end_exc -> (start, end_exc))
           start_seq end_exc_seq
-        : Time_slot_ds.t Seq.t ))
+        )
   |> OSeq.merge
