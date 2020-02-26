a837c0fc0337c8368cdb5a1edb16fe1ad56cbccb
Darren Ldl <darrenldldev@gmail.com>
2020-02-27 00:57:49 +1100

    Progress on time

--- lib/time.ml
+++ lib/time.ml
@@ -56,3 +56,9 @@ let wday_of_mday ~year ~month ~mday =
         }
   in
   tm.tm_wday
+
+let get_time_utc_sec () : int64 =
+  Unix.time () |> Int64.of_float
+
+let get_time_utc_min () : int64 =
+  (get_time_utc_sec ()) /^ 60L
