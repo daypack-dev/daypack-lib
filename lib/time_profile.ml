a60e1513d083d1fbc6c11af77d11d031c2c90c9c
Darren Ldl <darrenldldev@gmail.com>
2020-03-01 15:16:09 +1100

    Profile

--- lib/time_profile.ml
+++ lib/time_profile.ml
@@ -36,10 +36,12 @@ let matching_time_slots_of_periods ~start ~end_exc (periods : period list) :
       OSeq.map2 (fun start end_exc -> (start, end_exc)) start_seq end_exc_seq)
   |> OSeq.merge
 
-let matching_time_slots_of_data ~start ~end_exc ( data : data) : Time_slot_ds.t Seq.t =
+let matching_time_slots_of_data ~start ~end_exc (data : data) :
+  Time_slot_ds.t Seq.t =
   matching_time_slots_of_periods ~start ~end_exc data.periods
 
-let matching_time_slots_of_profile ~start ~end_exc ((_id, data) : t) : Time_slot_ds.t Seq.t =
+let matching_time_slots_of_profile ~start ~end_exc ((_id, data) : t) :
+  Time_slot_ds.t Seq.t =
   matching_time_slots_of_periods ~start ~end_exc data.periods
 
 module Serialize = struct
@@ -53,8 +55,7 @@ module Serialize = struct
   let pack_data (data : data) : Time_profile_t.data =
     { periods = pack_periods data.periods }
 
-  let pack_profile ((id, data) : t) : Time_profile_t.t =
-    (id, pack_data data)
+  let pack_profile ((id, data) : t) : Time_profile_t.t = (id, pack_data data)
 end
 
 module Deserialize = struct
@@ -68,6 +69,5 @@ module Deserialize = struct
   let unpack_data (data : Time_profile_t.data) : data =
     { periods = unpack_periods data.periods }
 
-  let unpack_profile ((id, data) : Time_profile_t.t) : t =
-    (id, unpack_data data)
+  let unpack_profile ((id, data) : Time_profile_t.t) : t = (id, unpack_data data)
 end
--- lib/time_profile_store.ml
+++ lib/time_profile_store.ml
@@ -1,16 +1,15 @@
 type t = Time_profile.data String_map.t
 
-let matching_time_slots_of_profile ~start ~end_exc ~(profile : string) (t : t) : Time_slot_ds.t Seq.t option =
+let matching_time_slots_of_profile ~start ~end_exc ~(profile : string) (t : t) :
+  Time_slot_ds.t Seq.t option =
   String_map.find_opt profile t
-|>
-  Option.map (fun profile ->
-    Time_profile.matching_time_slots_of_data ~start ~end_exc profile
-    )
+  |> Option.map (fun profile ->
+      Time_profile.matching_time_slots_of_data ~start ~end_exc profile)
 
 module Serialize = struct
   let pack_store (t : t) : Time_profile_store_t.t =
-    t |>
-    String_map.to_seq
+    t
+    |> String_map.to_seq
     |> Seq.map Time_profile.Serialize.pack_profile
     |> List.of_seq
 end
