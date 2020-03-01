300144e620969107f1eb9a70289ffd8a33010997
Darren Ldl <darrenldldev@gmail.com>
2020-03-01 17:01:58 +1100

    Added memoization for Time_profile_store.matching_time_slots_of_profile

--- lib/time_profile_store.ml
+++ lib/time_profile_store.ml
@@ -1,10 +1,19 @@
 type t = Time_profile.data String_map.t
 
-let matching_time_slots_of_profile ~start ~end_exc ~(profile : string) (t : t) :
-  Time_slot_ds.t Seq.t option =
-  String_map.find_opt profile t
-  |> Option.map (fun profile ->
-      Time_profile.matching_time_slots_of_data ~start ~end_exc profile)
+let matching_time_slots_of_profile =
+  let cache : (string, Time_slot_ds.t list) Hashtbl.t = Hashtbl.create 100 in
+  (fun ~start ~end_exc ~(profile : string) (t : t) :
+    Time_slot_ds.t list option ->
+    match Hashtbl.find_opt cache profile with
+    | None ->
+      String_map.find_opt profile t
+      |> Option.map (fun data ->
+          let time_slots = Time_profile.matching_time_slots_of_data ~start ~end_exc data |> List.of_seq in
+          Hashtbl.add cache profile time_slots;
+          time_slots
+        )
+    | Some time_slots -> Some time_slots
+  )
 
 module Serialize = struct
   let pack_store (t : t) : Time_profile_store_t.t =
