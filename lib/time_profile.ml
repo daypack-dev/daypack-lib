1a8844578eab0c39f6cb30572506ba2a9f926004
Darren Ldl <darrenldldev@gmail.com>
2020-03-01 12:31:01 +1100

    Formatting

--- lib/time_profile.ml
+++ lib/time_profile.ml
@@ -36,30 +36,24 @@ let matching_time_slots_of_periods ~start ~end_exc (periods : period list) :
 
 module Serialize = struct
   let pack_period (start, end_exc) : Time_profile_t.period =
-    (Time_pattern.Serialize.pack_pattern start,
-     Time_pattern.Serialize.pack_pattern end_exc
-    )
+    ( Time_pattern.Serialize.pack_pattern start,
+      Time_pattern.Serialize.pack_pattern end_exc )
 
   let pack_periods (periods : period list) : Time_profile_t.period list =
     List.map pack_period periods
 
   let pack_data (data : data) : Time_profile_t.data =
-    {
-      periods = pack_periods data.periods
-    }
+    { periods = pack_periods data.periods }
 end
 
 module Deserialize = struct
   let unpack_period (start, end_exc) : period =
-    (Time_pattern.Deserialize.unpack_pattern start,
-     Time_pattern.Deserialize.unpack_pattern end_exc
-    )
+    ( Time_pattern.Deserialize.unpack_pattern start,
+      Time_pattern.Deserialize.unpack_pattern end_exc )
 
   let unpack_periods (periods : Time_profile_t.period list) : period list =
     List.map unpack_period periods
 
   let unpack_data (data : Time_profile_t.data) : data =
-    {
-      periods = unpack_periods data.periods
-    }
+    { periods = unpack_periods data.periods }
 end
