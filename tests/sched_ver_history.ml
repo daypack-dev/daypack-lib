7eca9861ca4dde28c9905a43a2b7bdd67f5140df
Darren Ldl <darrenldldev@gmail.com>
2020-03-02 19:44:21 +1100

    Fixing sched ver history dir serialization test

--- tests/sched_ver_history.ml
+++ tests/sched_ver_history.ml
@@ -38,9 +38,13 @@ let qc_read_from_dir_is_inverse_of_write_to_dir =
     ~name:"qc_read_from_dir_is_inverse_of_write_to_dir"
     sched_ver_history
     (fun sched_ver_history ->
-       let dir = "qc_read_from_dir_is_inverse_of_write_to_dir" in
-       Daypack_lib.Sched_ver_history.Serialize.write_to_dir ~dir sched_ver_history |> Stdlib.Result.get_ok;
+       let dir = Core.Filename.temp_dir "daypack" "sched_ver_history" in
+       (match Daypack_lib.Sched_ver_history.Serialize.write_to_dir ~dir sched_ver_history with
+        | Ok () -> ()
+        | Error msg -> failwith msg
+       );
        let sched_ver_history' = Daypack_lib.Sched_ver_history.Deserialize.read_from_dir ~dir |> Stdlib.Result.get_ok in
+       FileUtil.rm ~recurse:true [ dir ];
        Daypack_lib.Sched_ver_history.Equal.equal sched_ver_history sched_ver_history'
     )
 
