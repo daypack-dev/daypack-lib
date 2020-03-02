e549c2b8943852e4bd83c790c46da86f3e99cf4e
Darren Ldl <darrenldldev@gmail.com>
2020-03-03 00:09:06 +1100

    Progress on time_profile test module

--- tests/time_profile.ml
+++ tests/time_profile.ml
@@ -22,8 +22,24 @@
   print_endline "let suite = [";
   List.iter (fun (name, _, _, _, _, _) ->
       Printf.printf "%s;\n" (unpack_is_inverse_of_pack_test_name name);
-    ) unpack_pack_store_list;
+    ) unpack_pack_list;
   print_endline "]";
 *)
+
+let qc_unpack_is_inverse_of_pack_period =
+  QCheck.Test.make ~count:5000 ~name:"qc_unpack_is_inverse_of_pack_period"
+    QCheck.(list_of_size Gen.(int_bound 100) (pair time_pattern time_pattern))
+    (fun l ->
+       let x = l |> List.to_seq |> Daypack_lib.String_map.of_seq in
+       let y =
+         x
+         |> Daypack_lib.Time_profile_store.Serialize.pack_store
+         |> Daypack_lib.Time_profile_store.Deserialize.unpack_store
+       in
+       Daypack_lib.Time_profile_store.Equal.equal
+         (fun x y -> compare x y = 0)
+         x y)
+
+let suite = [ qc_unpack_is_inverse_of_pack_period ]
 
 (*$*)
