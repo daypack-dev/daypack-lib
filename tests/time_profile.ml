39525390016a531363f7c4b10a359e0734358c5a
Darren Ldl <darrenldldev@gmail.com>
2020-03-03 00:21:59 +1100

    Progress on time profile tests

--- tests/time_profile.ml
+++ tests/time_profile.ml
@@ -1,45 +1,27 @@
-(*$ #use "tests/serialization_related.cinaps";;
-  let unpack_pack_list = [
-    ("period",
-     "(pair time_pattern time_pattern)",
-     "Daypack_lib.String_map.of_seq",
-     "Daypack_lib.Time_profile_store.Serialize.pack_store",
-     "Daypack_lib.Time_profile_store.Deserialize.unpack_store",
-     "Daypack_lib.Time_profile_store.Equal.equal"
-    )
-  ] in
-
-  List.iter (fun (name, inner_typ_gen, f_of_seq, f_pack, f_unpack, f_equal) ->
-      print_unpack_is_inverse_of_pack_test_store
-        ~name
-        ~inner_typ_gen
-        ~f_of_seq
-        ~f_pack
-        ~f_unpack
-        ~f_equal)
-    unpack_pack_list;
-
-  print_endline "let suite = [";
-  List.iter (fun (name, _, _, _, _, _) ->
-      Printf.printf "%s;\n" (unpack_is_inverse_of_pack_test_name name);
-    ) unpack_pack_list;
-  print_endline "]";
-*)
+open Test_utils
 
 let qc_unpack_is_inverse_of_pack_period =
   QCheck.Test.make ~count:5000 ~name:"qc_unpack_is_inverse_of_pack_period"
-    QCheck.(list_of_size Gen.(int_bound 100) (pair time_pattern time_pattern))
-    (fun l ->
-       let x = l |> List.to_seq |> Daypack_lib.String_map.of_seq in
-       let y =
-         x
-         |> Daypack_lib.Time_profile_store.Serialize.pack_store
-         |> Daypack_lib.Time_profile_store.Deserialize.unpack_store
+    QCheck.(pair time_pattern time_pattern)
+    (fun p ->
+       let p' =
+         p
+         |> Daypack_lib.Time_profile.Serialize.pack_period
+         |> Daypack_lib.Time_profile.Deserialize.unpack_period
        in
-       Daypack_lib.Time_profile_store.Equal.equal
-         (fun x y -> compare x y = 0)
-         x y)
+       p = p')
 
-let suite = [ qc_unpack_is_inverse_of_pack_period ]
+let qc_unpack_is_inverse_of_pack_data =
+  QCheck.Test.make ~count:5000 ~name:"qc_unpack_is_inverse_of_pack_data"
+    QCheck.(list (pair time_pattern time_pattern))
+    (fun periods ->
+       let d = Daypack_lib.Time_profile.{ periods } in
+       let d' =
+         d
+         |> Daypack_lib.Time_profile.Serialize.pack_data
+         |> Daypack_lib.Time_profile.Deserialize.unpack_data
+       in
+       d = d')
 
-(*$*)
+let suite =
+  [ qc_unpack_is_inverse_of_pack_period; qc_unpack_is_inverse_of_pack_data ]
