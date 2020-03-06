ccd8b6419afb4579921009fb9ba27b0e60639b59
Darren Ldl <darrenldldev@gmail.com>
2020-03-06 17:42:15 +1100

    Formatting

--- lib/sched_ver_history.ml
+++ lib/sched_ver_history.ml
@@ -148,15 +148,13 @@ module Maybe_append_to_head = struct
          let task_seg_place_seq =
            Sched.Agenda.find_task_seg_place_seq_by_task_id task_id hd
          in
-         let no_task_seg_place_s_recorded =
-           OSeq.is_empty task_seg_place_seq
-         in
+         let no_task_seg_place_s_recorded = OSeq.is_empty task_seg_place_seq in
          let no_task_inst_progress_recorded =
            let task_inst_progress_sets =
              Sched.Task_inst.find_task_inst_ids_any_by_task_id task_id hd
              |> Seq.map (fun task_inst_id ->
-                 Sched.Progress.find_task_inst_progress_chunk_set task_inst_id hd
-               )
+                 Sched.Progress.find_task_inst_progress_chunk_set task_inst_id
+                   hd)
              |> Seq.filter (fun s -> not (Int64_int64_set.is_empty s))
            in
            OSeq.is_empty task_inst_progress_sets
@@ -165,8 +163,8 @@ module Maybe_append_to_head = struct
            let task_seg_progress_sets =
              Sched.Task_seg.find_task_seg_ids_any_by_task_id task_id hd
              |> Seq.map (fun task_seg_id ->
-                 Sched.Progress.find_task_seg_progress_chunk_set task_seg_id hd
-               )
+                 Sched.Progress.find_task_seg_progress_chunk_set task_seg_id
+                   hd)
              |> Seq.filter (fun s -> not (Int64_int64_set.is_empty s))
            in
            OSeq.is_empty task_seg_progress_sets
@@ -177,10 +175,11 @@ module Maybe_append_to_head = struct
            |> Sched.Sched_req.remove_pending_sched_req_by_task_id task_id
            |> Sched.Sched_req.remove_sched_req_record_by_task_id task_id
          in
-         if no_task_seg_place_s_recorded
-         && no_task_inst_progress_recorded
-         && no_task_seg_progress_recorded then
-           ((), Replace_head hd')
+         if
+           no_task_seg_place_s_recorded
+           && no_task_inst_progress_recorded
+           && no_task_seg_progress_recorded
+         then ((), Replace_head hd')
          else
            let hd' =
              hd'
