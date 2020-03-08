b9535e8470c1687f113e46303cb91a31ff3ba7a4
Darren Ldl <darrenldldev@gmail.com>
2020-03-09 01:28:58 +1100

    Progress on cli

--- cli/list_cmd.ml
+++ cli/list_cmd.ml
@@ -3,9 +3,12 @@ open Cmdliner
 let task_arg =
   Arg.(value & flag & info ["task"])
 
-let run (list_task : bool) =
-  let context = Context.load () in
-  Printf.printf "task : %b\n" list_task
+let run (list_task : bool) : unit =
+  match Context.load () with
+  | Error msg -> print_endline msg
+  | Ok context ->
+    let hd = Daypack_lib.Sched_ver_history.Read.get_head context.sched_ver_history in
+    ()
 
 let cmd =
   Term.(const run $ task_arg),
