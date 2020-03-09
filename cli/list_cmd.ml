52a46f98867142733cc19117c57a8c628fa49ef9
Darren Ldl <darrenldldev@gmail.com>
2020-03-10 00:24:10 +1100

    Progress on cli

--- cli/list_cmd.ml
+++ cli/list_cmd.ml
@@ -9,7 +9,10 @@ let task_inst_arg =
 let task_seg_arg =
   Arg.(value & flag & info ["task-seg"])
 
-let run (list_task : bool) (list_task_inst : bool) (list_task_seg : bool) : unit =
+let sched_req_arg =
+  Arg.(value & flag & info ["sched-req"])
+
+let run (list_task : bool) (list_task_inst : bool) (list_task_seg : bool) (list_sched_req : bool) : unit =
   match Context.load () with
   | Error msg -> print_endline msg
   | Ok context ->
@@ -34,8 +37,15 @@ let run (list_task : bool) (list_task_inst : bool) (list_task_seg : bool) : unit
              Daypack_lib.Task_ds.Print.debug_print_task_seg task_seg;
           )
           (Daypack_lib.Sched.Task_seg.To_seq.task_seg_seq_all hd)
-      )
+      );
+    if list_sched_req then (
+      Seq.iter
+        (fun sched_req ->
+           Daypack_lib.Sched_req_ds.Print.debug_print_sched_req sched_req;
+        )
+        (Daypack_lib.Sched.Sched_req.To_seq.pending_sched_req_seq hd)
+    )
 
 let cmd =
-  Term.(const run $ task_arg $ task_inst_arg $ task_seg_arg),
+  Term.(const run $ task_arg $ task_inst_arg $ task_seg_arg $ sched_req_arg),
   Term.info "list"
