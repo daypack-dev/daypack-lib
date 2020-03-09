d2e5d3da6e44a897fdab026814a864a07e5822dc
Darren Ldl <darrenldldev@gmail.com>
2020-03-09 20:35:05 +1100

    Formatting

--- lib/sched_ver_history.ml
+++ lib/sched_ver_history.ml
@@ -29,10 +29,7 @@ let map_head (f : Sched.sched -> 'a * head_choice) (t : t) : 'a =
     ret
 
 module Read = struct
-  let get_head (t : t) : Sched.sched =
-    map_head
-      (fun s -> (s, Do_nothing))
-      t
+  let get_head (t : t) : Sched.sched = map_head (fun s -> (s, Do_nothing)) t
 end
 
 module In_place_head = struct
