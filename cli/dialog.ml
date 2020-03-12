3fd0845074be80e46ad63f2e693276137459a7ee
Darren Ldl <darrenldldev@gmail.com>
2020-03-12 13:19:41 +1100

    Progress on cli

--- cli/dialog.ml
+++ cli/dialog.ml
@@ -2,3 +2,14 @@ let ask (type a b) ~(prompt : string) (f : string -> (a, b) result) : (a, b) res
   Printf.printf "%s : " prompt;
   let s = read_line () in
   f s
+
+let ask_pick_choice (type a) ~(prompt : string) (choices : (string * a) list) : a =
+  Printf.printf "%s :\n" prompt;
+  List.iter
+    (fun (s, _) ->
+       Printf.printf "  %s\n" s;
+    ) choices;
+  ask ~prompt:"Please enter choice" (fun s ->
+      Ok (List.assoc s choices)
+    )
+  |> Result.get_ok
