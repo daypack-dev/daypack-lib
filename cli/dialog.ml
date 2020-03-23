b44ed019fd2d8390480811c68a0ff0503a00c81a
Darren Ldl <darrenldldev@gmail.com>
2020-03-24 00:09:34 +1100

    Progress on cli dialog

--- cli/dialog.ml
+++ cli/dialog.ml
@@ -1,14 +1,18 @@
-let ask (type a) ~(prompt : string) (f : string -> (a, string) result) : a =
-  let rec aux prompt f =
-    Printf.printf "%s : " prompt;
-    let s = read_line () in
-    match f s with
+let loop_until_success (type a) (f : unit -> (a, string) result) : a =
+  let rec aux () =
+    match f () with
     | Ok x -> x
     | Error msg ->
       Printf.printf "Error : %s\n" msg;
-      aux prompt f
+      aux ()
   in
-  aux prompt f
+  aux ()
+
+let ask (type a) ~(prompt : string) (f : string -> (a, string) result) : a =
+  loop_until_success (fun () ->
+      Printf.printf "%s : " prompt;
+      let s = read_line () in
+      f s)
 
 let ask_pick_choice (type a) ~(prompt : string) (choices : (string * a) list) :
   a =
@@ -33,6 +37,33 @@ let ask_pick_choice (type a) ~(prompt : string) (choices : (string * a) list) :
        | [ (_k, v) ] -> Ok v
        | _ -> Error "Input is too ambiguous and matches multiple choices")
 
+let ask_int ~(prompt : string) : int =
+  ask ~prompt (fun s ->
+      try int_of_string s |> Result.ok with Failure msg -> Error msg)
+
+let ask_int64 ~(prompt : string) : int64 =
+  ask ~prompt (fun s ->
+      try Int64.of_string s |> Result.ok with Failure msg -> Error msg)
+
+let ask_time ~(prompt : string) : int64 =
+  loop_until_success (fun () ->
+      let pat =
+        ask ~prompt Daypack_lib.Time_pattern.Interpret_string.of_string
+      in
+      match
+        Daypack_lib.Time_pattern.next_match_int64
+          ~search_years_ahead:Config.time_pattern_search_years_ahead
+          ~start:(Daypack_lib.Time.cur_unix_time_min ())
+          pat
+      with
+      | None -> Error "Failed to find matching start time"
+      | Some time -> Ok time)
+
+let ask_time_slot ~(prompt : string) : int64 * int64 =
+  let start = ask_time ~prompt:(prompt ^ " (start)") in
+  let end_exc = ask_time ~prompt:(prompt ^ " (end exc)") in
+  (start, end_exc)
+
 let ask_sched_req_data_unit ~(task_inst_id : Daypack_lib.Task_ds.task_inst_id) :
   (Daypack_lib.Sched_req_ds.sched_req_data_unit, string) result =
   let sched_req_choice =
@@ -47,26 +78,11 @@ let ask_sched_req_data_unit ~(task_inst_id : Daypack_lib.Task_ds.task_inst_id) :
       ]
   in
   match sched_req_choice with
-  | `Fixed -> (
-      let start_pat =
-        ask ~prompt:"Enter start time"
-          Daypack_lib.Time_pattern.Interpret_string.of_string
-      in
-      match
-        Daypack_lib.Time_pattern.next_match_int64
-          ~search_years_ahead:Config.time_pattern_search_years_ahead
-          ~start:(Daypack_lib.Time.cur_unix_time_min ())
-          start_pat
-      with
-      | None -> Error "Failed to find matching start time"
-      | Some start ->
-        let duration =
-          ask ~prompt:"Enter duration (minutes)" (fun s ->
-              try Int64.of_string s |> Result.ok
-              with Failure msg -> Error msg)
-        in
-        Ok
-          (Daypack_lib.Sched_req_data_unit_skeleton.Fixed
-             { task_seg_related_data = (task_inst_id, duration); start }) )
+  | `Fixed ->
+    let start = ask_time ~prompt:"Enter start time" in
+    let duration = ask_int64 ~prompt:"Enter duration (minutes)" in
+    Ok
+      (Daypack_lib.Sched_req_data_unit_skeleton.Fixed
+         { task_seg_related_data = (task_inst_id, duration); start })
   | `Shift | `Split_and_shift | `Split_even | `Time_share | `Push_toward | _ ->
     failwith "Not implemented"
