efca3851fa96735b9fa00ca88eff099eb690e585
Darren Ldl <darrenldldev@gmail.com>
2020-03-26 16:58:11 +1100

    Formatting

--- lib/misc_utils.ml
+++ lib/misc_utils.ml
@@ -3,6 +3,4 @@ let prefix_string_match (choices : (string * 'a) list) (s : string) :
   let regexp = Str.regexp_case_fold s in
   choices
   |> List.filter (fun (k, _) ->
-      try
-        Str.search_forward regexp k 0 = 0
-      with Not_found -> false)
+      try Str.search_forward regexp k 0 = 0 with Not_found -> false)
