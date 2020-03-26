d9752be0946aa7437e1bb508a4d95c473d5b8fac
Darren Ldl <darrenldldev@gmail.com>
2020-03-26 23:15:55 +1100

    Fixing int64 and int32 * int32 conversion

--- lib/misc_utils.ml
+++ lib/misc_utils.ml
@@ -6,9 +6,10 @@ let prefix_string_match (choices : (string * 'a) list) (s : string) :
       try Str.search_forward regexp k 0 = 0 with Not_found -> false)
 
 let int64_to_int32_int32 (x : int64) : int32 * int32 =
-  (Int64.to_int32 (Int64.shift_right_logical x 32), Int64.to_int32 x)
+  (Int64.shift_right_logical x 32 |> Int64.to_int32,
+   x |> Int64.to_int32)
 
 let int32_int32_to_int64 ((x, y) : int32 * int32) : int64 =
   let left = Int64.shift_left (Int64.of_int32 x) 32 in
-  let right = Int64.of_int32 y in
+  let right = Int64.logand 0x00000000FFFFFFFFL (Int64.of_int32 y) in
   Int64.logor left right
