26dc04d6bb6fd1c53e026cfdfbc011bf643db4a1
Darren Ldl <darrenldldev@gmail.com>
2020-03-31 02:49:55 +1100

    Formatting

--- cli/agenda_cmd.ml
+++ cli/agenda_cmd.ml
@@ -29,31 +29,34 @@ let run (list_free_time_slots : bool) : unit =
     else
       let places_within_period =
         Daypack_lib.Sched.Agenda.To_seq.task_seg_place_uncompleted ~start
-          ~end_exc
-          ~include_task_seg_place_partially_within_time_period:true hd
+          ~end_exc ~include_task_seg_place_partially_within_time_period:true
+          hd
         |> OSeq.take Config.agenda_display_task_seg_place_count
         |> List.of_seq
       in
       let places =
-        if List.length places_within_period < Config.agenda_display_task_seg_place_count then
+        if
+          List.length places_within_period
+          < Config.agenda_display_task_seg_place_count
+        then
           Daypack_lib.Sched.Agenda.To_seq.task_seg_place_uncompleted ~start
             ~include_task_seg_place_partially_within_time_period:true hd
           |> OSeq.take Config.agenda_display_task_seg_place_count
           |> List.of_seq
-        else
-          places_within_period
+        else places_within_period
       in
-      List.iter (fun (task_seg_id, place_start, place_end_exc) ->
-          let start_str =
-            Daypack_lib.Time.Print.date_string_of_time
-              ~display_in_time_zone:`Local place_start
-          in
-          let end_exc_str =
-            Daypack_lib.Time.Print.date_string_of_time
-              ~display_in_time_zone:`Local place_end_exc
-          in
-          Printf.printf "| %s - %s | %s\n" start_str end_exc_str
-            (Daypack_lib.Task_ds.string_of_task_seg_id task_seg_id))
+      List.iter
+        (fun (task_seg_id, place_start, place_end_exc) ->
+           let start_str =
+             Daypack_lib.Time.Print.date_string_of_time
+               ~display_in_time_zone:`Local place_start
+           in
+           let end_exc_str =
+             Daypack_lib.Time.Print.date_string_of_time
+               ~display_in_time_zone:`Local place_end_exc
+           in
+           Printf.printf "| %s - %s | %s\n" start_str end_exc_str
+             (Daypack_lib.Task_ds.string_of_task_seg_id task_seg_id))
         places
 
 let cmd = (Term.(const run $ free_time_slots_arg), Term.info "agenda")
