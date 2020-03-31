0f3e194aab570aba131be80480cdfe4400cd4e80
Darren Ldl <darrenldldev@gmail.com>
2020-03-31 23:01:27 +1100

    Added Duration module

---
+++ lib/duration.ml
@@ -0,0 +1,13 @@
+
+module To_string = struct
+  let human_readable_string_of_duration (duration : int64) : string =
+    let minutes = Int64.div duration 60L in
+    let hours = Int64.div minutes 60L in
+    let days = Int64.div hours 24L in
+    if days > 0L then
+      Printf.sprintf "%Ld days %Ld hours %Ld mins" days (Int64.rem hours 24L)
+        (Int64.rem minutes 60L)
+    else if hours > 0L then
+      Printf.sprintf "%Ld hours %Ld mins" hours (Int64.rem minutes 60L)
+    else Printf.sprintf "%Ld mins" minutes
+end
--- lib/time.ml
+++ lib/time.ml
@@ -335,15 +335,4 @@ module Print = struct
   let debug_print_time ?(indent_level = 0) ~(display_in_time_zone : time_zone)
       (time : int64) : unit =
     print_string (debug_string_of_time ~indent_level ~display_in_time_zone time)
-
-  let human_readable_string_of_duration (duration : int64) : string =
-    let minutes = Int64.div duration 60L in
-    let hours = Int64.div minutes 60L in
-    let days = Int64.div hours 24L in
-    if days > 0L then
-      Printf.sprintf "%Ld days %Ld hours %Ld mins" days (Int64.rem hours 24L)
-        (Int64.rem minutes 60L)
-    else if hours > 0L then
-      Printf.sprintf "%Ld hours %Ld mins" hours (Int64.rem minutes 60L)
-    else Printf.sprintf "%Ld mins" minutes
 end
