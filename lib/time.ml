2bcb46e09a101508c40d05f4ae1d4bfff910b622
Darren Ldl <darrenldldev@gmail.com>
2020-04-01 19:12:04 +1100

    Code refactoring

--- lib/time.ml
+++ lib/time.ml
@@ -92,7 +92,7 @@ let tm_int_of_month (month : month) : int =
 let human_int_of_month (month : month) : int =
   tm_int_of_month month + 1
 
-let month_of_int (x : int) : month =
+let month_of_tm_int (x : int) : month =
   match x with
   | 0 -> `Jan
   | 1 -> `Feb
@@ -109,7 +109,7 @@ let month_of_int (x : int) : month =
   | _ -> failwith "Invalid month int"
 
 let month_of_human_int (x : int) : month =
-  month_of_int (x - 1)
+  month_of_tm_int (x - 1)
 
 let cal_month_of_month (month : month) : CalendarLib.Calendar.month =
   match month with
