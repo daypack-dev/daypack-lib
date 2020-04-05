7ad9bbb54b21d3484bcfc94884a159431cd1c3b2
Darren Ldl <darrenldldev@gmail.com>
2020-04-05 23:49:24 +1000

    Progress on time expr

--- lib/time_expr.ml
+++ lib/time_expr.ml
@@ -25,9 +25,15 @@ let add_hour_minute_to_time_pattern (e : hour_minute_expr) (pat : Time_pattern.t
     minutes = e.minute :: pat.minutes;
   }
 
-(* let days_of_day_range_expr (e : day_range_expr) : day_expr list =
- *   match e with
- *   | Weekday_range  *)
+let days_of_day_range_expr (e : day_range_expr) : day_expr list =
+  match e with
+  | Weekday_range (start, end_inc) ->
+    Time.weekday_list_of_weekday_range ~start ~end_inc
+    |> List.map (fun x -> Weekday x)
+  | Month_day_range (start, end_inc) ->
+    OSeq.(start -- end_inc)
+    |> Seq.map (fun x -> Month_day x)
+    |> List.of_seq
 
 let paired_time_patterns_list_of_time_slots_expr (e : time_slots_expr) : (Time_pattern.t * Time_pattern.t) list =
   match e with
@@ -39,8 +45,15 @@ let paired_time_patterns_list_of_time_slots_expr (e : time_slots_expr) : (Time_p
         (add_hour_minute_to_time_pattern hm_start pat,
          add_hour_minute_to_time_pattern hm_end_exc pat)
       )
-  (* | Hour_minutes_of_day_range { hour_minutes; days } ->
-   *   let hm_start, hm_end_exc = paired_hour_minute_of_range_expr hour_minutes in *)
+  | Hour_minutes_of_day_range { hour_minutes; days } ->
+    let hm_start, hm_end_exc = paired_hour_minute_of_range_expr hour_minutes in
+    days
+    |> days_of_day_range_expr
+    |> List.map time_pattern_of_day_expr
+    |> List.map (fun pat ->
+        (add_hour_minute_to_time_pattern hm_start pat,
+         add_hour_minute_to_time_pattern hm_end_exc pat)
+      )
   | _ -> failwith "Unimplemented"
 
 (* let time_range_exprs_of_complex_time_range_expr (e : complex_time_range_expr) : time_range_expr list =
