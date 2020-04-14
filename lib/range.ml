5085cdcac78cea2a3333f646bb1da0cc0a36a166
Darren Ldl <darrenldldev@gmail.com>
2020-04-14 20:46:45 +1000

    Updated Range

--- lib/range.ml
+++ lib/range.ml
@@ -12,7 +12,7 @@ let flatten_seq ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a Seq.
       OSeq.(start_int -- end_inc_int)
       |> Seq.map of_int
     else
-      Seq.empty
+      raise (Invalid_argument "End is before start")
   | `Range_exc (start, end_exc) ->
     let start_int = to_int start in
     let end_exc_int = to_int end_exc in
@@ -20,7 +20,7 @@ let flatten_seq ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a Seq.
       OSeq.(start_int --^ end_exc_int)
       |> Seq.map of_int
     else
-      Seq.empty
+      raise (Invalid_argument "End is before start")
 
 let flatten_list ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a list =
   flatten_seq ~of_int ~to_int t
