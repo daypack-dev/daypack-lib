833241e8e13dbb66d4e3237f608e34373541da49
Darren Ldl <darrenldldev@gmail.com>
2020-04-14 18:55:12 +1000

    Updated flatten functions

--- lib/range.ml
+++ lib/range.ml
@@ -3,7 +3,7 @@ type 'a t =
   | `Range_exc of 'a * 'a
   ]
 
-let flatten ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a list =
+let flatten_seq ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a Seq.t =
   match t with
   | `Range_inc (start, end_inc) ->
     let start_int = to_int start in
@@ -11,15 +11,17 @@ let flatten ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a list =
     if start_int <= end_inc_int then
       OSeq.(start_int -- end_inc_int)
       |> Seq.map of_int
-      |> List.of_seq
     else
-      []
+      Seq.empty
   | `Range_exc (start, end_exc) ->
     let start_int = to_int start in
     let end_exc_int = to_int end_exc in
     if start_int <= end_exc_int then
       OSeq.(start_int --^ end_exc_int)
       |> Seq.map of_int
-      |> List.of_seq
     else
-      []
+      Seq.empty
+
+let flatten_list ~(of_int : int -> 'a) ~(to_int : 'a -> int) (t : 'a t) : 'a list =
+  flatten_seq ~of_int ~to_int t
+  |> List.of_seq
