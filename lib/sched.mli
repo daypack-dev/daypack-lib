ebc961e57d27b334cbe772421dae87e10a354de1
Darren Ldl <darrenldldev@gmail.com>
2020-04-16 23:01:37 +1000

    Added missing line for find_task_seg_any_opt in sched.mli

--- lib/sched.mli
+++ lib/sched.mli
@@ -166,6 +166,9 @@ module Task_seg : sig
     val find_task_seg_discarded_opt :
       Task_ds.task_seg_id -> sched -> Task_ds.task_seg_size option
 
+    val find_task_seg_any_opt :
+      Task_ds.task_seg_id -> sched -> Task_ds.task_seg_size option
+
     val find_task_seg_ids_by_task_inst_id :
       Task_ds.task_inst_id -> sched -> Task_ds.task_seg_id Seq.t
 
