5c5b1618c27222022d004c294fbe6254f6f18c09
Darren Ldl <darrenldldev@gmail.com>
2020-04-17 20:18:33 +1000

    Updated overdue notification

--- cli/notification.ml
+++ cli/notification.ml
@@ -14,14 +14,35 @@ let display_overdue_task_segs (context : Context.t) : unit =
   let hd =
     Daypack_lib.Sched_ver_history.Read.get_head context.sched_ver_history
   in
-  let overdue_task_segs =
-    Daypack_lib.Sched.Overdue.get_overdue_task_segs ~deadline:(Daypack_lib.Time.Current.cur_unix_time ())
+  let overdue_task_seg_places =
+    Daypack_lib.Sched.Overdue.get_overdue_task_seg_places ~deadline:(Daypack_lib.Time.Current.cur_unix_time ())
       hd
     |> List.of_seq
   in
-  let count = List.length overdue_task_segs in
+  let count = List.length overdue_task_seg_places in
   if count = 0 then print_endline "  - No overdue task segments"
-  else Printf.printf "  - Overdue task segments: %d\n" count
+  else (
+    print_endline "  - Overdue task segments:";
+    List.iter
+      (fun (task_seg_id, place_start, place_end_exc) ->
+         let open Daypack_lib.Task_ds in
+         let task_id = Daypack_lib.Task_ds.Id.task_id_of_task_seg_id task_seg_id in
+         let task_data = Daypack_lib.Sched.Task.Find.find_task_uncompleted_opt
+             task_id hd |> Option.get
+         in
+         let start_str =
+           Daypack_lib.Time.To_string.date_time_string_of_time
+             ~display_in_time_zone:`Local place_start
+         in
+         let end_exc_str =
+           Daypack_lib.Time.To_string.date_time_string_of_time
+             ~display_in_time_zone:`Local place_end_exc
+         in
+         Printf.printf "    - | %s - %s | %s | %s\n"
+           start_str end_exc_str
+           (Id.string_of_task_seg_id task_seg_id)
+           task_data.name)
+      overdue_task_seg_places)
 
 let display_todos (context : Context.t) : unit =
   let hd =
