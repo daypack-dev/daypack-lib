82816422c8c1ed418954e65b19c43ee3d26ff7b9
Darren Ldl <darrenldldev@gmail.com>
2020-04-17 15:11:28 +1000

    Adding ID matching functions to Task_ds

--- lib/task_ds.ml
+++ lib/task_ds.ml
@@ -119,6 +119,20 @@ module Id = struct
   let task_inst_id_of_task_seg_id ((id1, id2, id3, _id4, _id5) : task_seg_id) :
     task_inst_id =
     (id1, id2, id3)
+
+  let task_inst_id_matches_task_id (task_inst_id : task_inst_id) (task_id : task_id) : bool =
+    task_id = task_id_of_task_inst_id task_inst_id
+
+  let task_seg_id_matches_task_id (task_seg_id : task_seg_id) (task_id : task_id) : bool =
+    task_id = task_id_of_task_seg_id task_seg_id
+
+  let task_seg_id_matches_task_inst_id (task_seg_id : task_seg_id) (task_inst_id : task_inst_id) : bool =
+    task_inst_id = task_inst_id_of_task_seg_id task_seg_id
+
+  let task_seg_id_matches_task_seg_id_ignore_sub_id
+      ((id1, id2, id3, id4, _id5) : task_seg_id)
+      ((id1', id2', id3', id4', _id5') : task_seg_id) : bool =
+    id1 = id1' && id2 = id2' && id3 = id3' && id4 = id4'
 end
 
 let task_seg_alloc_req_sum_length reqs =
