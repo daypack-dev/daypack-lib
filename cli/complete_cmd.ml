8a6edc6732cbbceb30ce08a37b9990ec77da650a
Darren Ldl <darrenldldev@gmail.com>
2020-04-22 23:08:52 +1000

    Added complete cmd --task-inst

--- cli/complete_cmd.ml
+++ cli/complete_cmd.ml
@@ -2,7 +2,7 @@ open Cmdliner
 
 let task_arg = Arg.(value & flag & info [ "task" ])
 
-let task_inst_arg = Arg.(value & flag & info [ "task-place" ])
+let task_inst_arg = Arg.(value & flag & info [ "task-inst" ])
 
 let task_seg_arg = Arg.(value & flag & info [ "task-seg" ])
 
@@ -35,6 +35,37 @@ let run (complete_task : bool) (complete_task_inst : bool)
           then
             Daypack_lib.Sched_ver_history.In_place_head.Task.Move
             .move_task_to_completed task_id context.sched_ver_history
+            |> Dialog.report_action_record );
+    ( if complete_task_inst then
+        let task_inst_id =
+          Dialog.ask_task_inst_id ~indent_level:0 ~exists_in_sched:(Some hd)
+        in
+        match
+          Daypack_lib.Sched.Task_inst.Find.find_task_inst_any_with_status_opt
+            task_inst_id hd
+          |> Option.get
+        with
+        | _, `Completed ->
+          print_endline "Task instance is marked as completed already"
+        | _, `Discarded -> print_endline "Task instance is marked as discarded"
+        | _, `Uncompleted ->
+          let task_id =
+            Daypack_lib.Task_ds.Id.task_id_of_task_inst_id task_inst_id
+          in
+          let task_data =
+            Daypack_lib.Sched.Task.Find.find_task_any_opt task_id hd
+            |> Option.get
+          in
+          if
+            Dialog.ask_yn ~indent_level:0
+              ~prompt:
+                (Printf.sprintf
+                   "Really mark instance under task \"%s\" as completed?"
+                   task_data.name)
+            = `Yes
+          then
+            Daypack_lib.Sched_ver_history.In_place_head.Task_inst.Move
+            .move_task_inst_to_completed task_inst_id context.sched_ver_history
             |> Dialog.report_action_record );
     ( if complete_task_seg then
         let task_seg_id =
