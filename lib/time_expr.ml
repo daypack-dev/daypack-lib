861660a349357bc06139b55a47e986e35afe95fb
Darren Ldl <darrenldldev@gmail.com>
2020-04-25 04:29:23 +1000

    Code refactoring

--- lib/time_expr.ml
+++ lib/time_expr.ml
@@ -227,19 +227,7 @@ end
 module Interpret_string = struct
   open Angstrom
 
-  let alpha_string : string t =
-    take_while1 (function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false)
-
-  let integer : int t =
-    take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
-
-  let space : unit t = skip_while (function ' ' | '\t' -> true | _ -> false)
-
-  let comma = char ','
-
-  let dot = char '.'
-
-  let hyphen = char '-'
+  open Parser_components
 
   let to_string = string_ci "to"
 
