c545a95609c5e1180d63212f663f15865601faf3
Darren Ldl <darrenldldev@gmail.com>
2020-05-08 18:52:39 +1000

    Fixed comment wording

--- src/time_slot.ml
+++ src/time_slot.ml
@@ -1,7 +1,7 @@
 type t = int64 * int64
 
 let compare (x1, y1) (x2, y2) =
-  (* lexicographic product *)
+  (* lexicographical order *)
   if x1 < x2 || (x1 = x2 && y1 < y2) then -1
   else if x1 = x2 && y1 = y2 then 0
   else 1
