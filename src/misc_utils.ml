ac47a95f67998a8df90832034f19f47e285fd2b4
Darren Ldl <darrenldldev@gmail.com>
2020-07-03 17:24:20 +1000

    Switched to Re

--- src/misc_utils.ml
+++ src/misc_utils.ml
@@ -1,9 +1,9 @@
 let prefix_string_match (choices : (string * 'a) list) (s : string) :
   (string * 'a) list =
-  let regexp = Str.regexp_case_fold s in
+  let regexp = Re.Str.regexp_case_fold s in
   choices
   |> List.filter (fun (k, _) ->
-      try Str.search_forward regexp k 0 = 0 with Not_found -> false)
+      try Re.Str.search_forward regexp k 0 = 0 with Not_found -> false)
 
 let int32_int32_of_int64 (x : int64) : int32 * int32 =
   (Int64.shift_right_logical x 32 |> Int64.to_int32, x |> Int64.to_int32)
