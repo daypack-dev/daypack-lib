cd80c62e87e9f601743c07f9823a6d932ffe6b2e
Darren Ldl <darrenldldev@gmail.com>
2020-07-11 00:16:29 +1000

    Formatting

--- src/time_expr.ml
+++ src/time_expr.ml
@@ -679,17 +679,17 @@ module Of_string = struct
   end
 
   let inter : (Time_expr_ast.t -> Time_expr_ast.t -> Time_expr_ast.t) t =
-    ( string "&&"
-         *> return (fun a b -> Time_expr_ast.Time_binary_op (Inter, a, b)) )
+    string "&&"
+    *> return (fun a b -> Time_expr_ast.Time_binary_op (Inter, a, b))
 
   let union : (Time_expr_ast.t -> Time_expr_ast.t -> Time_expr_ast.t) t =
-    ( string "||"
-         *> return (fun a b -> Time_expr_ast.Time_binary_op (Union, a, b)) )
+    string "||"
+    *> return (fun a b -> Time_expr_ast.Time_binary_op (Union, a, b))
 
   let round_robin_select :
     (Time_expr_ast.t -> Time_expr_ast.t -> Time_expr_ast.t) t =
-    ( string ">>"
-         *> return (fun a b -> Time_expr_ast.Time_round_robin_select [ a; b ]) )
+    string ">>"
+    *> return (fun a b -> Time_expr_ast.Time_round_robin_select [ a; b ])
 
   let flatten_round_robin_select (e : Time_expr_ast.t) : Time_expr_ast.t =
     let open Time_expr_ast in
